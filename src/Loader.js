2aff7380929487dd33848a7528126c5d248cac5f
jelka2008 <jelka2008@rambler.ru>
2019-10-30 13:35:08 +0300

    first commit

---
+++ src/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => 
+    <div style={{  display: 'flex', justifyContent: 'center', margin: '.5rem'}} className="lds-facebook">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
